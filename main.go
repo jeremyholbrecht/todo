adc5eb0de3f3ff0591079ba1aa6819b841385f24
jeremyholbrecht <Griffley@proton.me>
2024-01-29 16:18:39 +0100

    feat: add http server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type task struct {
+	todo   string
+	isDone bool
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Bar")
+}
+
+func todo(w http.ResponseWriter, req *http.Request) {
+	t := task{todo: "clean", isDone: false}
+	fmt.Fprint(w, t)
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	http.HandleFunc("/foo", foo)
+	http.HandleFunc("/todo", todo)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println("error connecting to localhost")
+	}
 }
