8e8446057ad7394c4bcceb983a4478be750adb62
jeremyholbrecht <Griffley@proton.me>
2024-02-08 13:14:00 +0100

    fixed typing error in comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // TODO: make it so that each done button is properly associated with it's task
-// currently it doesn't matter which button we click it will go from top to button instead of it's associated task
+// currently it doesn't matter which button we click it will go from top to buttom instead of it's associated task
 
 const taskDoneButtons = document.querySelectorAll('.taskDoneButton');
 
